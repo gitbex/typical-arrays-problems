b1009cfee77b34701e9124365e0c7168410aa172
Bekhruz Mirrakhimov <bexruz@gmail.com>
2020-03-09 23:12:10 +0500

    init: task pass

--- src/index.js
+++ src/index.js
@@ -1,12 +1,25 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } else {
+    return Math.min(...array)
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } else {
+    return Math.max(...array)
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0){
+    return 0;
+  } else {
+    let avarage = array.reduce((ava, item) => ava + item, 0)
+     return avarage / array.length
+  }
 }
